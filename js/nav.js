0613b06cc600b73e654cec2da3ce84ba8565e557
BladeHiker <duanjihang@live.com>
2020-10-13 13:33:38 +0800

    增加回顶按钮

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
 document.getElementById('hamburger-toggler').checked = false
+document.getElementsByClassName('toTopBtn')[0].style.opacity = 0
 var pre_backgroundImage
 document.getElementById('hamburger-toggler').onchange = (ev) => {
 
@@ -14,7 +15,19 @@ document.getElementById('hamburger-toggler').onchange = (ev) => {
 window.addEventListener('scroll', (e) => {
     if (document.getElementById('hamburger-toggler').checked == true) return;
     var nav_op = document.documentElement.scrollTop / document.documentElement.clientHeight * 3
+    if (nav_op > 2.5) {
+        document.getElementsByClassName('toTopBtn')[0].style.opacity = 1
+    } else {
+        document.getElementsByClassName('toTopBtn')[0].style.opacity = 0
+    }
     nav_op = nav_op > 0.8 ? 0.8 : nav_op
     console.log(nav_op)
     document.getElementsByTagName("nav")[0].style.backgroundImage = "linear-gradient(180deg, #404244c0, rgba(40, 42, 44, " + nav_op * nav_op + "))";
 }, false)
+
+function toTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
