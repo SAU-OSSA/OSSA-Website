e04dac93c94456eb6d180eae5c973332a38fc9d9
BladeHiker <duanjihang@live.com>
2020-10-13 12:31:38 +0800

    Update toolset.js

--- js/toolset.js
+++ js/toolset.js
@@ -1,5 +1,1 @@
-window.addEventListener('scroll', (e) => {
-    var nav_op = document.documentElement.scrollTop / document.documentElement.clientHeight * 3
-    nav_op = nav_op > 0.8 ? 0.8 : nav_op
-    document.getElementsByTagName("nav")[0].style.backgroundImage = "linear-gradient(180deg, #000000c0, rgba(40, 42, 44, " + nav_op * nav_op + "))";
-}, false)
+
